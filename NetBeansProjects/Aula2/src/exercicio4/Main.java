d4f1546463dbbb3b6dedb26b236b5296a33f49e2
Elton Diel Reisdorfer <19221080@facdombosco.com.br>
2020-03-03 22:14:00 -0300

    Aula 2.

--- NetBeansProjects/Aula2/src/exercicio4/Main.java
+++ NetBeansProjects/Aula2/src/exercicio4/Main.java
@@ -5,6 +5,8 @@
  */
 package exercicio4;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 19221080
@@ -17,14 +19,18 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        Calculadora cal = new Calculadora(10, 2);
+        JOptionPane.showMessageDialog(null, "Bem vindo a calculadora!");
+        double num1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro número:"));
+        double num2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo número:"));
+        
+        Calculadora cal = new Calculadora(num1, num2);
 
         System.out.println("Soma: " + cal.somaNumeros());
         System.out.println("Subt: " + cal.subtrNumeros());
         System.out.println("Mult: " + cal.multNumeros());
         System.out.println("Divi: " + cal.divNumeros());
 
-        System.out.println("Div F: " + cal.divNumeros(50, 5));
+        System.out.println("Div F: " + cal.divNumeros(num1, num2));
     }
 
 }
